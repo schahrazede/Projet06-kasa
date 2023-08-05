e8c847b513cd5aaa1cd8f5ecd3b66e21adc207fe
schahrazede <schahrazedemohamedvall@gmail.com>
2023-08-05 15:28:37 +0200

    last commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 import "../styles/pages/Error404.scss";
 import Gallery from "../components/Gallery";
 
